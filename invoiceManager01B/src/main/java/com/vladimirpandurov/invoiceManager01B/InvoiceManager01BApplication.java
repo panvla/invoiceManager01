bc2548bc8f13d70c6543caff7941de21427954a7
panvla <pandvlado@gmail.com>
2023-06-10 11:56:18 +0200

    added initial project with some applicaton.yml configuration.closes #1.

---
+++ invoiceManager01B/src/main/java/com/vladimirpandurov/invoiceManager01B/InvoiceManager01BApplication.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.invoiceManager01B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceManager01BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceManager01BApplication.class, args);
+	}
+
+}
---
+++ invoiceManager01B/src/test/java/com/vladimirpandurov/invoiceManager01B/InvoiceManager01BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.invoiceManager01B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManager01BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
